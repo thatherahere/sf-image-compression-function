a00131a13a660faad8e41c5aee52b80986c72a82
thatherahere <mastanametal@gmail.com>
2022-07-27 23:03:04 +0530

    Initial commit...

--- functions/sfimagecompression/src/httpService.js
+++ functions/sfimagecompression/src/httpService.js
@@ -1,7 +1,7 @@
 import https from 'https';
 
 export class HttpService {
-    static async request(options, body) {
+    static async downloadContentVersion(options, body, logger) {
         return new Promise((resolve, reject) => {
             const req = https.request(options, (res) => {
                 if (res.statusCode !== 200) {
@@ -18,6 +18,47 @@ export class HttpService {
                     data.push(chunk);
                 }).on('end', () => {
                     resolve(Buffer.concat(data));
+                });
+            });
+
+            req.on('error', (err) => {
+                reject(
+                    new Error(
+                        `Error in GET request: HTTP ${res.statusCode} ${
+                            res.statusMessage
+                        } - ${JSON.stringify(options)}`,
+                        { cause: err }
+                    )
+                );
+            });
+
+            if (body) {
+                req.write(body);
+            }
+
+            req.end();
+        });
+    }
+
+    static async uploadCompressedContent(options, body, logger) {
+        return new Promise((resolve, reject) => {
+            const req = https.request(options, (res) => {
+                if (res.statusCode !== 200 && res.statusCode !== 201) {
+                    reject(
+                        new Error(
+                            `Error in POST request: HTTP ${res.statusCode} ${
+                                res.statusMessage
+                            } - ${JSON.stringify(options)}`
+                        )
+                    );
+                }
+                let data = '';
+                res.on('data', (chunk) => {
+                    data = data + chunk.toString();
+                });
+
+                res.on('end', () => {
+                    resolve(JSON.parse(data));
                 });
             });
 
--- functions/sfimagecompression/src/index.js
+++ functions/sfimagecompression/src/index.js
@@ -11,20 +11,46 @@
  *                 to a given execution of a function.
  */
 
- import 'dotenv/config';
- import { HttpService } from './httpService.js';
- import { sharp } from "sharp";
- 
-export default async function (event, context, logger) {
-    logger.info(`Invoking Sfcompressfiles with payload ${JSON.stringify(event.data || {})}`);
+import { HttpService } from './httpService.js';
+import sharp from "sharp"; 
 
-    try {
-        const files = event.data || [];
-        const salesforceDocIds = [];
-        for (const file of files) {
-            salesforceDocIds.push(await getFileContent(file, context, logger));
+const OriginalError = global.Error;
+class Error extends OriginalError {
+    constructor(msg, options) {
+        super(msg);
+        if (options?.cause) {
+            this.cause = options.cause;
         }
-        return { salesforceDocIds };
+    }
+
+    toString() {
+        let value = this.message;
+        if (this.cause) {
+            value += `\nCaused by: ${this.cause}`;
+        }
+        return value;
+    }
+}
+
+const sharpConfiguration = { "quality": 50 };
+
+export default async function (event, context, logger) {
+    try {
+        const files = event.data.files || [];
+        let sharpOptions = event.data.sharpConfiguration || sharpConfiguration;
+        const successCVIds = [];
+        const failedCVIds = [];
+        for (const file of files) {
+            try{
+                const contentBody = await readContentVersionData( file, context );
+                const bufferResponse = await compressContentBody( file, contentBody, sharpOptions ); 
+                await updateDocumentWithCompressedContent( file, bufferResponse, context, logger );
+                successCVIds.push( file.contentVersionId );
+            }catch(err){
+                failedCVIds.push( file.contentVersionId );
+            }
+        }
+        return { contentVersionIds };
     } catch (err) {
         const newError = new Error(`Failed to process documents`, {
             cause: err
@@ -34,48 +60,59 @@ export default async function (event, context, logger) {
     }
 }
 
-async function getFileContent( file, context, logger ){
-    try {
-        // Download document from Salesforce
-        const fileContent = await downloadSalesforceFile(
-            file.contentVersionId,
-            context
-        );
-        sharp(fileContent)
-            .webp({ quality: 20 })
-            .toBuffer()
-            .then(data => { /* Send to salesforce */ 
-            
-            }).catch(err => { logger.info("Error: "+JSON.stringify(err)) });
-    
-    }catch (err) {
-        throw new Error(`Failed to process file ${JSON.stringify(file)}`, {
-            cause: err
-        });
-    }
-}
-
-
-/**
- * Downloads a document using Salesforce REST API
- * @param {string} contentVersionId
- * @param {*} context function context
- * @returns byte buffer that contains document content
- */
-async function downloadSalesforceFile(contentVersionId, context) {
+async function readContentVersionData( file, context ){
     const { apiVersion, domainUrl } = context.org;
     const { accessToken } = context.org.dataApi;
     const options = {
         hostname: domainUrl.substring(8), // Remove https://
-        path: `/services/data/v${apiVersion}/sobjects/ContentVersion/${contentVersionId}/VersionData`,
+        path: `/services/data/v${apiVersion}/sobjects/ContentVersion/${file.contentVersionId}/VersionData`,
         method: 'GET',
         headers: {
             Authorization: `Bearer ${accessToken}`
         }
     };
     try {
-        return await HttpService.request(options);
+        return await HttpService.downloadContentVersion(options);
     } catch (err) {
         throw new Error(`Failed to download Salesforce doc`, { cause: err });
+    }
+}  
+
+async function compressContentBody( file, contentBody, sharpOptions ){
+    if( file.fileExtesion === "jpg" || file.fileExtesion === "jpeg" ){
+        return await sharp(contentBody).jpeg(sharpOptions).toBuffer();
+    }else if( file.fileExtesion === "png" ){
+        return await sharp(contentBody).png(sharpOptions).toBuffer();
+    }
+}
+
+async function updateDocumentWithCompressedContent(file, bufferData, context, logger) {
+    const { apiVersion, domainUrl } = context.org;
+    const { accessToken } = context.org.dataApi;
+    const options = {
+        hostname: domainUrl.substring(8), // Remove https://
+        path: `/services/data/v${apiVersion}/sobjects/ContentVersion`,
+        method: 'POST',
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json' 
+        }
+    };
+
+    const contentBody = {
+        "Title": file.title,
+        "PathOnClient": file.title,
+        "VersionData": bufferData.toString('base64'),
+        "ContentDocumentId" : file.contentDocumentId,
+        "IsMajorVersion" : true,
+        "ReasonForChange" : "Compressed file"
+      };
+
+    try {
+        const response = await HttpService.uploadCompressedContent(options, JSON.stringify( contentBody ), logger);
+        logger.info( "Upload Response: "+JSON.stringify( response ) );
+        return response;
+    } catch (err) {
+        throw new Error(`Failed to create new salesforce content version`, { cause: err });
     }
 }
