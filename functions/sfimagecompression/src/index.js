f2c2a3abea78c1ad3f86627eb91649a2004a44b9
thatherahere <mastanametal@gmail.com>
2022-07-27 23:20:00 +0530

    Initial commit...

--- functions/sfimagecompression/src/index.js
+++ functions/sfimagecompression/src/index.js
@@ -50,7 +50,7 @@ export default async function (event, context, logger) {
                 failedCVIds.push( file.contentVersionId );
             }
         }
-        return { contentVersionIds };
+        return { "successCVIds" : successCVIds, "failedCVIds" : failedCVIds };
     } catch (err) {
         const newError = new Error(`Failed to process documents`, {
             cause: err
