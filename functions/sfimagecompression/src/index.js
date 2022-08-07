165c2a49c40ae5e112430676bc1b2d4e589dd04b
thatherahere <mastanametal@gmail.com>
2022-08-07 22:27:39 +0530

    Removed additional logs...

--- functions/sfimagecompression/src/index.js
+++ functions/sfimagecompression/src/index.js
@@ -40,8 +40,6 @@ const sharpConfiguration = { "quality": 50 };
  */
 export default async function (event, context, logger) {
     try {
-        logger.info("UserId: "+context.org.user.id);
-        logger.info("Username: "+context.org.user.username);
         logger.info("Request body: "+JSON.stringify(event.data));
         const files = event.data.files || [];
         let sharpOptions = event.data.sharpConfiguration || sharpConfiguration;
